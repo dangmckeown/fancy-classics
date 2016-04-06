1ae4201b0f338fb70adba11adceedb46d15aee2c
Dan McKeown <dangmckeown@users.noreply.github.com>
2016-04-06 15:12:53 +0100

    Update xml.php

--- xml.php
+++ xml.php
@@ -4,14 +4,6 @@ $renewals = array();
 $xml = simplexml_load_file("http://cachepricefeeds.williamhill.com/openbet_cdn?action=template&template=getHierarchyByMarketType&classId=13&marketSort=--&filterBIR=N");
 $output = $xml->response->williamhill->class->type;
 
-foreach ($type as $ty){
-  foreach ($ty->market as $mar){
-    $op++;
-    echo "Operation $op";
-    $renewals[] = $mar;
-  }
-}
-
-print_r($renewals);
+print_r($output);
 
 ?>
