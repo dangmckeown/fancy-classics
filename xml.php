203b31244461f76f32cf5c66905e515a13d54e2e
Dan McKeown <dangmckeown@users.noreply.github.com>
2016-04-06 15:00:04 +0100

    Update xml.php
    Revert to beginning and try print_r method

--- xml.php
+++ xml.php
@@ -1,39 +1,8 @@
 <?php
 
-
-//invalid xml file
-$xmlparser = xml_parser_create();
-
-// open a file and read data
-$fp = fopen("http://cachepricefeeds.williamhill.com/openbet_cdn?action=template&template=getHierarchyByMarketType&classId=13&marketSort=--&filterBIR=N", 'r');
-$xmldata = fread($fp, 4096);
-
-xml_parse_into_struct($xmlparser,$xmldata,$values);
-
-xml_parser_free($xmlparser);
-print_r($values);
-
-
-/*
-$dom = new DOMDocument();
-$dom->simplexml_load_file("http://cachepricefeeds.williamhill.com/openbet_cdn?action=template&template=getHierarchyByMarketType&classId=13&marketSort=--&filterBIR=N");
-#$dom->loadHTMLfile('http://remoteDomain/thispage.html');
-$classics=$dom->getElementById('749');
-
-$market = $classics->market;
-
-foreach ($market as $mark)
-{
-
-  
-$ps = $mark->participant;
-
-foreach($ps as $p)
-{
-    echo $p['name']." - ".$p['odds']."<br />";
-}
-
-} //end foreach market
-*/
+$xml = simplexml_load_file("http://cachepricefeeds.williamhill.com/openbet_cdn?action=template&template=getHierarchyByMarketType&classId=13&marketSort=--&filterBIR=N");
+foreach ($xml as $x){
+echo "<hr />"; 
+print_r($x);
 
 ?>
