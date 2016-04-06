a5be6c75abe7c9935bda9ad8ae1f2b1449203a76
Dan McKeown <dangmckeown@users.noreply.github.com>
2016-04-06 13:00:30 +0100

    Update xml.php
    Try to expand each market

--- xml.php
+++ xml.php
@@ -2,26 +2,18 @@
 
 $xml = simplexml_load_file("http://cachepricefeeds.williamhill.com/openbet_cdn?action=template&template=getHierarchyByMarketType&classId=13&marketSort=--&filterBIR=N");
 
-$data = $xml->response->williamhill->class->type;
+$data = $xml->response->williamhill->class->type->market;
 
-$classics = $data->market;
-
-echo $classics;
-
-/*
-foreach($classics as $classic)
-echo "<h1>" . $classic->name . "</h1>";
+foreach ($market as $mark)
 {
     
-$ps = $classic->participant;
+$ps = $mark->participant;
 
 foreach($ps as $p)
 {
     echo $p['name']." - ".$p['odds']."<br />";
 }
 
-} //end foreach classics
-
-*/
+} //end foreach market
 
 ?>
