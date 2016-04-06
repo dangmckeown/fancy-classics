c6b394c71d571d3883bc14773a0c08b2d7ca0209
Dan McKeown <dangmckeown@users.noreply.github.com>
2016-04-06 15:14:10 +0100

    Update xml.php

--- xml.php
+++ xml.php
@@ -2,7 +2,7 @@
 $op = 0;
 $renewals = array();
 $xml = simplexml_load_file("http://cachepricefeeds.williamhill.com/openbet_cdn?action=template&template=getHierarchyByMarketType&classId=13&marketSort=--&filterBIR=N");
-$output = $xml->response->williamhill->class->type;
+$output = $xml->response->williamhill->class;
 
 print_r($output);
 
