30098c1876f0590b28a5360c45b8282e980211a3
Dan McKeown <dangmckeown@users.noreply.github.com>
2016-04-06 15:58:06 +0100

    Update xml.php

--- xml.php
+++ xml.php
@@ -5,7 +5,8 @@ $xml = simplexml_load_file("http://cachepricefeeds.williamhill.com/openbet_cdn?a
 $market = $xml->response->williamhill->class->type[1]->market;
 
 foreach($market as $mark){
-  echo "<h1>" . $mark->attributes()->name . "</h1>\n";
+  $title = preg_match("/.*2016$/",$mark->attributes()->name);
+  echo "<h1>" . $title . "</h1>\n";
   echo "<table>\n<thead>\n<th>Horse</th> <th>Price</th>\n</thead>\n<tbody>\n";
  
  
