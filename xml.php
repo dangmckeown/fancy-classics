53889939373a148d2c2d4b29b33033522e2dc646
Dan McKeown <dangmckeown@users.noreply.github.com>
2016-04-06 14:18:00 +0100

    Update xml.php

--- xml.php
+++ xml.php
@@ -1,10 +1,10 @@
 <?php
-
+/*
 $dom = new DOMDocument();
 $dom->simplexml_load_file("http://cachepricefeeds.williamhill.com/openbet_cdn?action=template&template=getHierarchyByMarketType&classId=13&marketSort=--&filterBIR=N");
 #$dom->loadHTMLfile('http://remoteDomain/thispage.html');
 $classics=$dom->getElementById('749');
-/*
+
 $market = $classics->market;
 
 foreach ($market as $mark)
