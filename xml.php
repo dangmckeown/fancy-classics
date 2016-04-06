7fc5f7a66c839fcf025a705f5c9bdf87af73eed2
Dan McKeown <dangmckeown@users.noreply.github.com>
2016-04-06 12:29:05 +0100

    Update xml.php
    Try xmlrpc_decode with imported $xml value

--- xml.php
+++ xml.php
@@ -1,6 +1,6 @@
 <?php
 
-$source = "http://cachepricefeeds.williamhill.com/openbet_cdn?action=template&template=getHierarchyByMarketType&classId=13&marketSort=--&filterBIR=N";
+$source = simplexml_load_file("http://cachepricefeeds.williamhill.com/openbet_cdn?action=template&template=getHierarchyByMarketType&classId=13&marketSort=--&filterBIR=N");
 
 $xml = xmlrpc_decode($source);
 
