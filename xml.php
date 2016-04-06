4361949cc046d6e5f201aef9b233bb7586e53780
Dan McKeown <dangmckeown@users.noreply.github.com>
2016-04-06 12:24:33 +0100

    Update xml.php
    Try out xmlrpc_decode function

--- xml.php
+++ xml.php
@@ -1,12 +1,9 @@
 <?php
 
-$xml = simplexml_load_file("http://cachepricefeeds.williamhill.com/openbet_cdn?action=template&template=getHierarchyByMarketType&classId=13&marketSort=--&filterBIR=N");
-foreach ($xml as $xm){
-  foreach ($xm as $xl){
-    foreach ($xl as $x){
-      echo "<hr />"; 
-      var_dump($x);
-    }
-  }
-}
+$source = "http://cachepricefeeds.williamhill.com/openbet_cdn?action=template&template=getHierarchyByMarketType&classId=13&marketSort=--&filterBIR=N";
+
+$xml = xmlrpc_decode($source);
+
+var_dump($xml);
+
 ?>
