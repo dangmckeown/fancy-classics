37959a89bd67cc3f6e59ba246bec4df0681f22e0
Dan McKeown <dangmckeown@users.noreply.github.com>
2016-04-06 15:08:16 +0100

    Update xml.php
    Try adding data to $renewals array

--- xml.php
+++ xml.php
@@ -1,7 +1,15 @@
 <?php
 
+$renewals = array();
 $xml = simplexml_load_file("http://cachepricefeeds.williamhill.com/openbet_cdn?action=template&template=getHierarchyByMarketType&classId=13&marketSort=--&filterBIR=N");
+$output = $xml->response->williamhill->class->type;
 
-print_r($xml);
+foreach ($type as $ty){
+  foreach ($ty->market as $mar){
+    $renewals[] = $mar;
+  }
+}
+
+print_r($renewals);
 
 ?>
