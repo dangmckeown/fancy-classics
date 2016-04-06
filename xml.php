68431233f7e53cff4a1bd3e85421b45441dc6188
Dan McKeown <dangmckeown@users.noreply.github.com>
2016-04-06 15:00:51 +0100

    Update xml.php

--- xml.php
+++ xml.php
@@ -1,8 +1,7 @@
 <?php
 
 $xml = simplexml_load_file("http://cachepricefeeds.williamhill.com/openbet_cdn?action=template&template=getHierarchyByMarketType&classId=13&marketSort=--&filterBIR=N");
-foreach ($xml as $x){
-echo "<hr />"; 
-print_r($x);
+
+print_r($xml);
 
 ?>
